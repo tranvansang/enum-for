9b3a6c7adf89759b589ae01748c5089ea596b1a2
Tran Sang <tr@nsang.me>
2021-08-21 11:56:36 +0900

    rename test enums

--- test.ts
+++ test.ts
@@ -1,12 +1,12 @@
 const {getAllEnumKeys, getAllEnumValues, getAllEnumEntries} = require('./index')
 
-enum TestEnum1 {
+enum Enum1 {
 	a, b
 }
-enum TestEnum2 {
+enum Enum2 {
 	a = 'a', b = 1, c = 1, d = 'e'
 }
-enum TestEnum3 {
+enum Enum3 {
 	a = 'b',
 	c = 1,
 	'1a',
@@ -14,18 +14,18 @@ enum TestEnum3 {
 }
 describe('enum', () => {
 	test('all values', () => {
-		expect(getAllEnumKeys(TestEnum1).sort()).toEqual(['a', 'b'].sort())
-		expect(getAllEnumKeys(TestEnum2).sort()).toEqual(['a', 'b', 'c', 'd'].sort())
-		expect(getAllEnumKeys(TestEnum3).sort()).toEqual(['a', 'c', '1a', '1b'].sort())
+		expect(getAllEnumKeys(Enum1).sort()).toEqual(['a', 'b'].sort())
+		expect(getAllEnumKeys(Enum2).sort()).toEqual(['a', 'b', 'c', 'd'].sort())
+		expect(getAllEnumKeys(Enum3).sort()).toEqual(['a', 'c', '1a', '1b'].sort())
 	})
 	test('all keys', () => {
-		expect(getAllEnumValues(TestEnum1).sort()).toEqual([0, 1].sort())
-		expect(getAllEnumValues(TestEnum2).sort()).toEqual(['a', 1, 1, 'e'].sort())
-		expect(getAllEnumValues(TestEnum3).sort()).toEqual(['b', 1, 2, 'd'].sort())
+		expect(getAllEnumValues(Enum1).sort()).toEqual([0, 1].sort())
+		expect(getAllEnumValues(Enum2).sort()).toEqual(['a', 1, 1, 'e'].sort())
+		expect(getAllEnumValues(Enum3).sort()).toEqual(['b', 1, 2, 'd'].sort())
 	})
 	test('all entries', () => {
-		expect(getAllEnumEntries(TestEnum1).sort()).toEqual([['a', 0], ['b', 1]].sort())
-		expect(getAllEnumEntries(TestEnum2).sort()).toEqual([['a', 'a'], ['b', 1], ['c', 1], ['d', 'e']].sort())
-		expect(getAllEnumEntries(TestEnum3).sort()).toEqual([['a', 'b'], ['c', 1], ['1a', 2], ['1b', 'd']].sort())
+		expect(getAllEnumEntries(Enum1).sort()).toEqual([['a', 0], ['b', 1]].sort())
+		expect(getAllEnumEntries(Enum2).sort()).toEqual([['a', 'a'], ['b', 1], ['c', 1], ['d', 'e']].sort())
+		expect(getAllEnumEntries(Enum3).sort()).toEqual([['a', 'b'], ['c', 1], ['1a', 2], ['1b', 'd']].sort())
 	})
 })
